3c437a8c5c3385217091f52fd124108925e2a43b
codingmonkey <codingmonkey1900@gmail.com>
2019-08-18 14:40:17 -0700

    PMD integrated

--- reviewbot-worker/init.sh
+++ reviewbot-worker/init.sh
@@ -1,1 +1,10 @@
+mkdir -p /var/reviewbot/tools
+cd /var/reviewbot/tools
+wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.17.0/pmd-bin-6.17.0.zip
+unzip pmd-bin-6.17.0.zip
+rm pmd-bin-6.17.0.zip
+echo "export PATH=//var/reviewbot/tools/pmd-bin-6.17.0/bin/:$PATH">>~/.bashrc
+mkdir -p /etc/xdg/reviewbot/
+touch /etc/xdg/reviewbot/config.py
+echo "pmd_path = '/var/reviewbot/tools/pmd-bin-6.17.0/bin/run.sh'" >> /etc/xdg/reviewbot/config.py
 reviewbot worker -b amqp://reviewbot_worker:reviewbot_worker@"$RABBITMQ_HOST":5672/
