e1a257dfaab1c11c9f47b24ec5c588fe4cdfdc29
codingmonkey <codingmonkey1900@gmail.com>
2019-08-18 14:59:10 -0700

    Added java and checkstyle to reviewbot worker

--- reviewbot-worker/init.sh
+++ reviewbot-worker/init.sh
@@ -3,8 +3,17 @@ cd /var/reviewbot/tools
 wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.17.0/pmd-bin-6.17.0.zip
 unzip pmd-bin-6.17.0.zip
 rm pmd-bin-6.17.0.zip
-echo "export PATH=//var/reviewbot/tools/pmd-bin-6.17.0/bin/:$PATH">>~/.bashrc
+echo "export PATH=/var/reviewbot/tools/pmd-bin-6.17.0/bin/:$PATH">>~/.bashrc
 mkdir -p /etc/xdg/reviewbot/
 touch /etc/xdg/reviewbot/config.py
 echo "pmd_path = '/var/reviewbot/tools/pmd-bin-6.17.0/bin/run.sh'" >> /etc/xdg/reviewbot/config.py
+
+cd /var/reviewbot/tools
+mkdir checkstyle
+cd checkstyle
+wget -O checkstyle.jar https://search.maven.org/remotecontent?filepath=com/puppycrawl/tools/checkstyle/8.23/checkstyle-8.23.jar
+chmod +x checkstyle.jar
+echo "export PATH=/var/reviewbot/tools/checkstyle/checkstyle.jar:$PATH">>~/.bashrc
+echo "checkstyle_path = '/var/reviewbot/tools/checkstyle/checkstyle.jar'" >> /etc/xdg/reviewbot/config.py
+cd 
 reviewbot worker -b amqp://reviewbot_worker:reviewbot_worker@"$RABBITMQ_HOST":5672/
